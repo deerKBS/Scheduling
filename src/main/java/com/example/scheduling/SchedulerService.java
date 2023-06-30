c4daefcdfcccf9cee81ed297a8fb323e655e5ef3
deerKBS <deer980119@gmail.com>
2023-06-30 19:56:29 +0900

    scheduler project setting

---
+++ src/main/java/com/example/scheduling/SchedulerService.java
@@ -0,0 +1,18 @@
+package com.example.scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class SchedulerService {
+
+    @Scheduled(fixedDelay = 60000) // 1분마다 실행
+        public void run() {
+            SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+            Date date = new Date(System.currentTimeMillis());
+            System.out.println(formatter.format(date));
+    }
+}
